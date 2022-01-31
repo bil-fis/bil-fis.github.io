b45a8e8e353c0e480b3600cc6671364b170f4ffc
bil-fis <468840127@qq.com>
2022-01-31 16:04:46 +0800

    Site updated: 2022-01-31 16:04:44

--- apps/happynewyear/hpny.js
+++ apps/happynewyear/hpny.js
@@ -3,10 +3,31 @@
 // @bilibili: https://space.bilibili.com/586310538
 // b站bil_fis制作
 
-window.onload= function(){
-    setTimeout( function(){
+window.onload= function(){setTimeout("check()",200);}
+
+function check(){
+console.log('check已经执行');
+      //平台、设备和操作系统
+        var system = {
+            win: false,
+            mac: false,
+            xll: false,
+            ipad:false
+        };
+        //检测平台
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+        system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+        //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+        if (system.win || system.mac || system.xll||system.ipad) {
+        var audio = document.getElementById('audio');
+        audio.pause();
+            document.body.innerHTML="手机扫码打开：<br /> <img id='code'src='qrcode.png'/>";
+        } else {
         setup();
-    },1000);
+        }
 }
 
 function setup(){
