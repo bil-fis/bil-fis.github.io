4a25633999dd5319d934b1876f6ee1adee0cef93
bil-fis <468840127@qq.com>
2022-01-31 16:11:40 +0800

    Site updated: 2022-01-31 16:11:40

--- apps/happynewyear/hpny.js
+++ apps/happynewyear/hpny.js
@@ -14,13 +14,12 @@ console.log('check已经执行');
             xll: false,
             ipad:false
         };
-        //检测平台
+        //检测平台（废除）
         var p = navigator.platform;
         system.win = p.indexOf("Win") == 0;
         system.mac = p.indexOf("Mac") == 0;
         system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
         system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
-        //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
         if (system.win || system.mac || system.xll||system.ipad) {
         var audio = document.getElementById('audio');
         setup();
